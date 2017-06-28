7767bbc322ba9ad0d3c9bff13f5e88e6bf65668b
Ryan Turner <ryanthepit@gmail.com>
2017-06-28 16:46:33 -0700

    initial commit, created employer and employee Schema

---
+++ lab-ryan/model/employer.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const employerSchema = mongoose.Schema({
+  company: {type: String, required: true, unique: true},
+  name: {type: String, required: true},
+  employees: [{type: mongoose.Schema.Types.ObjectId, unique: true, ref: 'employee'}],
+});
+
+module.exports = mongoose.model('employer', employerSchema);
