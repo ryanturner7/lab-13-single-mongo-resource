8d538b341eacc3310a9263eaa9a8da4b257cd324
Ryan Turner <ryanthepit@gmail.com>
2017-07-02 20:52:58 -0700

    updated employee model

--- lab-ryan/model/employee.js
+++ lab-ryan/model/employee.js
@@ -1,20 +1,22 @@
 'use strict';
 
-const faker = require('faker');
-const mockEmployer = require('./../mock-employer.js');
-const Employee = require('../../model/employee.js');
+const mongoose = require('mongoose');
+const Employer = require('./employer.js')
 
-const mockEmployee = module.exports = {}
-
-mock.createOne = () => {
+const employeeSchema = mongoose.Schema({
   title: {type: String, required: true, unique: true},
   employer: [{type: mongoose.Schema.Types.ObjectId, ref: 'employer'}],
   name: {type: String, required: true},
-  age: {type: Number, required: true},
 });
 
 employeeSchema.pre('save', function(next){
   Employer.findById(this.employer)
+  .then(employer => {
+    let employeeIDSet = new Set(employer.employees);
+    employeeIDSet.add(this._id);
+    employer.employees = Array.from(employeeIDSet);
+    return employer.save();
+  })
   .then(() => next())
   .catch(() => next(new Error('Failed to create employee because employer does not exist.')));
 });
